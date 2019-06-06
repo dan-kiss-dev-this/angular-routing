c71ed9bb2b13b6b4b41f2ed0b948bc42e3bbdb8e
Dan Kiss <flowfast47@gmail.com>
2019-06-06 09:18:39 -0700

    practice routing

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+// import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -8,12 +10,18 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
+  server: { id: number; name: string; status: string };
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.router.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.router.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id']);
+    });
   }
-
 }
