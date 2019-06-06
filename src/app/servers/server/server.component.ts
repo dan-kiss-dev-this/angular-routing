541215e96a221440117f5ea8823a83fefcac20a0
Dan Kiss <flowfast47@gmail.com>
2019-06-06 10:35:55 -0700

    use query parameters to determine views

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
 // import { ActivatedRoute } from '@angular/router';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -14,14 +14,19 @@ export class ServerComponent implements OnInit {
 
   constructor(
     private serversService: ServersService,
-    private router: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
-    const id = +this.router.snapshot.params['id'];
+    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
-    this.router.params.subscribe((params: Params) => {
+    this.route.params.subscribe((params: Params) => {
       this.server = this.serversService.getServer(+params['id']);
     });
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
   }
 }
