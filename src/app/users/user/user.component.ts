d0554791457cae5e1315745bdc5ca01cc17e847a
Dan Kiss <flowfast47@gmail.com>
2019-06-06 08:32:08 -0700

    set up routing with query parameters and fragments

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
@@ -8,6 +9,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class UserComponent implements OnInit, OnDestroy {
   user: { id: number; name: string };
+  paramsSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -16,13 +18,13 @@ export class UserComponent implements OnInit, OnDestroy {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
-    this.route.params.subscribe((params: Params) => {
+    this.paramsSubscription = this.route.params.subscribe((params: Params) => {
       this.user.id = params['id'];
       this.user.name = params['name'];
     });
   }
 
   ngOnDestroy() {
-    
+    this.paramsSubscription.unsubscribe();
   }
 }
